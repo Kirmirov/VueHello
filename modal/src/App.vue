096d3cf63c370299fa8ef13af8a8fc2972f152ff
Kirmirov <aruter@yandex.ru>
2021-05-12 16:58:31 +0300

    begin new work

---
+++ modal/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <div class="wrapper-content">
+
+        <section>
+
+          <div class="container">
+            <button class="btn btnPrimary" @click="modalCondition = !modalCondition">Show modal</button>
+            <Modal title="First modal"
+                    v-if="modalCondition"
+                    @close="modalCondition = !modalCondition">
+
+              <div slot="body">
+                
+                <input type="text" name="" id="">
+              </div>
+            </Modal>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Modal from '@/components/Modal.vue'
+
+export default {
+  components: { // регистрация компонент
+    Modal
+  },
+  data () { // хранение переменных
+    return{
+      modalCondition: false
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
